97bc983b68457400776f113283906308fa5536cd
FreetimeFM <awesomea380@tutanota.com>
2022-01-11 10:42:45 +0000

    Replaced hello api with coffee

--- pages/api/coffee.js
+++ pages/api/coffee.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(418).json({
+    error: true,
+    message: "418: I'm a teapot"
+  })
 }
