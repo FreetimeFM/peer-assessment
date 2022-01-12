12b7b2c5bd18908e3a3823eb103026dd58d882a5
FreetimeFM <awesomea380@tutanota.com>
2022-01-12 18:45:23 +0000

    Finished login api

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,9 @@
 import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "../../lib/iron-session/session";
+import { getUserDetailsByEmail } from "../../lib/database";
 import { getErrorMessage } from "../../lib/errors";
+import bcryptjs from "bcryptjs";
+
 function createErrorPayload(errorCode) {
   return {
     error: true,
@@ -9,12 +13,36 @@ function createErrorPayload(errorCode) {
 }
 
 export default withIronSessionApiRoute(async (req, res) => {
-  try {
+  const { email, password } = await req.body;
 
-    const user = { isLoggedIn: true, details: details.result };
+  // If 'email' or 'password' is not seen in POST request body, then return error.
+  if (!email || !password) return res.status(400).json(createErrorPayload(104));
+
+  try {
+    const details = await getUserDetailsByEmail(email); // Fetches user details by email.
+
+    // If there has been a error.
+    if (details.error) {
+
+      // If the account doesn't exist.
+      if (details.error.description === "Set not found.") return res.status(403).json(createErrorPayload(101));
+
+      // Unknown error.
+      return res.status(500).json(createErrorPayload(100));
+    }
+
+    // Compare password input to hashed password.
+    bcryptjs.compare(password, details.result.data.password).then(res => {
+
+      // If passwords don't match then send error.
+      if (!res) return res.status(403).json(createErrorPayload(102));
+    })
+
+    // Saves session to browser.
+    const user = { isLoggedIn: true, created: Date.now(), details: details.result };
     req.session.user = user;
     await req.session.save();
-    res.json(user);
+    res.status(200).json({ name: details.result.data.name });
 
   } catch (error) {
     res.status(500).json(createErrorPayload(300));
