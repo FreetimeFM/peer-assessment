b98535502dc8c895d2f437ea93e7ab618466932c
FreetimeFM <awesomea380@tutanota.com>
2022-01-13 09:53:43 +0000

    Made login form more responsive

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ import fetchJson, { FetchError } from "../lib/iron-session/fetchJson";
 import {
   Button,
   Card,
+  CardHeader,
   CardBody,
   Container,
   Form,
   FormGroup,
   Input,
   Label,
-  CardHeader,
 } from "reactstrap"
 
 export default function Home() {
@@ -55,44 +55,50 @@ export default function Home() {
     <>
       <Meta />
 
-        <Container className="container-md" >
-          <div className="position-absolute top-50 start-50 translate-middle">
-            <Card className="shadow-lg b-5 rounded">
-              <CardHeader className="text-center p-5">
-                <h4>Welcome to the Peer Assessment System</h4>
-                <h5>Please enter your login details</h5>
-              </CardHeader>
+      <Container className="d-flex justify-content-center">
+        <div className="position-absolute top-50 translate-middle-y">
+          <Card className="shadow-lg b-5 rounded m-3">
+            <CardHeader className="text-center p-sm-5">
+              <h4>Welcome to the Peer Assessment System</h4>
+              <p className="mb-0">Please enter your login details</p>
+            </CardHeader>
 
-              <CardBody className="p-5 text-center">
-                <Form method="POST" onSubmit={onSubmitHandler} inline>
-                  <FormGroup floating>
-                    <Input
-                      id="emailField"
-                      name="email"
-                      placeholder=" "
-                      type="email"
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <Label for="emailField">Email</Label>
-                  </FormGroup>
-                  <FormGroup floating>
-                    <Input
-                      id="pwField"
-                      name="password"
-                      placeholder=" "
-                      type="password"
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Label for="pwField">Password</Label>
-                  </FormGroup>
+            <CardBody className="px-sm-5 py-sm-4 pb-0">
+              <Form method="POST" onSubmit={onSubmitHandler} inline>
+                <FormGroup floating>
+                  <Input
+                    id="emailField"
+                    name="email"
+                    placeholder=" "
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Label for="emailField">Email</Label>
+                </FormGroup>
+                <FormGroup floating>
+                  <Input
+                    id="pwField"
+                    name="password"
+                    placeholder=" "
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Label for="pwField">Password</Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Input type="checkbox" />
+                  <Label check>Remember Me</Label>
+                </FormGroup>
+
+                <FormGroup className="text-end">
                   <Button className="px-4 py-2">Login</Button>
-                </Form>
-              </CardBody>
-            </Card>
+                </FormGroup>
 
-          </div>
-        </Container>
-
+              </Form>
+            </CardBody>
+          </Card>
+        </div>
+      </Container>
     </>
   );
 }
