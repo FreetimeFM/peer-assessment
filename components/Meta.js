5e5194b4a8627875a96c001a1ed54570707b3eba
FreetimeFM <awesomea380@tutanota.com>
2022-01-14 18:48:53 +0000

    Added favicon link as parameter for Meta component

--- components/Meta.js
+++ components/Meta.js
@@ -1,17 +1,19 @@
 import Head from "next/head";
 
-const Meta = ({title}) => {
+const Meta = ({children, title, iconLink}) => {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content="A peer review assessment system." />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href={iconLink} />
+      {children}
     </Head>
   )
 }
 
 Meta.defaultProps = {
-  title: "Peer assessment system"
+  title: "Peer assessment system",
+  iconLink: "/favicon.ico"
 }
 
 export default Meta
