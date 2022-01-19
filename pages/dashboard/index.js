8f29ad6cd1041c3159320a9f9252336c2493f852
FreetimeFM <awesomea380@tutanota.com>
2022-01-19 20:19:01 +0000

    Use server side rendering for dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router"
 
-const index = () => {
+import { withIronSessionSsr } from "iron-session/next";
+import { sessionOptions } from "../../lib/iron-session/session";
+
+
+export default function index({ user }) {
+
+  const router = useRouter();
+
+  if (!user.isLoggedIn) {
+    router.push("/");
+  }
+
+
   return (
     <div>
       <h1>Aloha</h1>
@@ -8,4 +21,22 @@ const index = () => {
   )
 }
 
-export default index
+export const getServerSideProps = withIronSessionSsr(async function ({req, res,}) {
+  const user = req.session.user;
+
+  if (user === undefined) {
+    res.setHeader("location", "/");
+    res.statusCode = 302;
+    res.end();
+
+    return {
+      props: {
+        user: { isLoggedIn: false },
+      },
+    };
+  }
+
+  return {
+    props: { user: req.session.user },
+  };
+}, sessionOptions);
