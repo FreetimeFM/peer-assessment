a2cf51207d33f9310b14c2b845d055321a75236c
FreetimeFM <awesomea380@tutanota.com>
2022-01-20 10:27:39 +0000

    Created assessment list

---
+++ components/AssessmentList.js
@@ -0,0 +1,16 @@
+
+
+import { Card } from "semantic-ui-react";
+
+export default function AssessmentList({ assessments }) {
+
+  assessments = [
+    {
+      name: "Assessment 1",
+      module: "ACXXXXX - Module name"
+    }
+  ]
+
+  return (
+  )
+}
