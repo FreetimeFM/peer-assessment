5c5f84ca2e0b1525e42c194bc7fc20993768d54c
FreetimeFM <awesomea380@tutanota.com>
2022-02-01 18:03:32 +0000

    Added dynamic sidebar buttons

--- components/DashboardSidebar.js
+++ components/DashboardSidebar.js
@@ -3,23 +3,32 @@ import { Sidebar, Menu, Icon, MenuItem } from "semantic-ui-react";
 import useUser from "../lib/iron-session/useUser";
 import fetchJson from "../lib/iron-session/fetchJson";
 
-const DashboardSidebar = ({name}) => {
+export default function DashboardSidebar({ pages, currentPage, user, onPageClick }) {
+  const { mutateUser } = useUser();
+
   return (
-    <Sidebar as={Menu} direction="left" vertical visible={true}>
-      <MenuItem as="h2">
-        Peer Assessment System
+    <Sidebar as={Menu} direction="left" vertical visible>
+      <MenuItem style={{ marginBottom: 0 }}>
+        <h2>Peer Assessment System</h2>
+        <h4 style={{ marginTop: 10 }}>Hello, {user.name}.</h4>
       </MenuItem>
-      <Menu.Item as="a">
-        <Icon name="home" />
-        Home
-      </Menu.Item>
-      <Menu.Item as="a">
-      <Menu.Item as="a" onClick={async (e) => {
+      {
+        pages.map((page, index) =>
+        <SidebarButton
+          key={index}
+          iconName={page.iconName}
+          content={page.name}
+          active={currentPage === index}
+          onItemClick={_e => onPageClick(index)}
+        />)
+      }
+      <Menu.Item as="a" color="primary" onClick={async (e) => {
         e.preventDefault();
         mutateUser(
           await fetchJson("/api/logout", { method: "POST" }),
           false,
         );
+        sessionStorage.clear();
         location.href = "/";
       }}>
         <Icon name="log out" />
@@ -29,4 +38,18 @@ const DashboardSidebar = ({name}) => {
   );
 }
 
-export default DashboardSidebar
+function SidebarButton({ iconName, content, active, onItemClick }) {
+  return (
+    <MenuItem
+      as="a"
+      active={active}
+      onClick={e => {
+        e.preventDefault();
+        onItemClick();
+      }}
+    >
+      <Icon name={iconName} />
+      {content}
+    </MenuItem>
+  )
+}
