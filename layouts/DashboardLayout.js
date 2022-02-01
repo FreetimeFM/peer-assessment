f0ab13b935e66f0b93fd465b3b05ecffdfe4f699
FreetimeFM <awesomea380@tutanota.com>
2022-02-01 18:36:28 +0000

    Display sidebar in layout

--- layouts/DashboardLayout.js
+++ layouts/DashboardLayout.js
@@ -1,15 +1,16 @@
-import DashboardSidebar from "../components/DashboardSidebar"
-
-// import { Row, Col, Container } from "reactstrap"
 import { Sidebar } from "semantic-ui-react";
+
+import DashboardSidebar from "../components/DashboardSidebar";
 import PageHeader from "../components/PageHeader";
 
-export default function DashboardLayout({ children, user, pages, currentPage, onDashboardSidebarPageClick }) {
+export default function DashboardLayout({ children, username, pages, currentPage, onDashboardSidebarPageClick }) {
 
   const { name, subHeading, iconName } = pages[currentPage];
 
   return (
     <>
+      <DashboardSidebar username={username} pages={pages} currentPage={currentPage} isMobile={isMobile} onPageClick={onDashboardSidebarPageClick} />
+      <Sidebar.Pusher style={{ width: "calc(100% - 260px)", padding: "10px 20px" }}>
         <PageHeader heading={name} subHeading={subHeading} iconName={iconName} />
         <main>
           {children}
