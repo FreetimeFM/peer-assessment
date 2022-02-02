37b9c150ad7daf5530f10852d05eff30dc38e2f7
FreetimeFM <awesomea380@tutanota.com>
2022-02-02 18:46:50 +0000

    Created file for past assessments

---
+++ pages/dashboard/past.js
@@ -0,0 +1,60 @@
+import { withIronSessionSsr } from "iron-session/next";
+
+import { sessionOptions } from "../../lib/iron-session/session";
+import DashboardLayout from "../../layouts/DashboardLayout";
+import AssessmentList from "../../components/AssessmentList";
+
+export default function PastAssessments({ user, assessments }) {
+
+  return (
+    <DashboardLayout user={user}>
+      <AssessmentList assessments={assessments} userType={user.userType}/>
+    </DashboardLayout>
+  )
+
+}
+
+export const getServerSideProps = withIronSessionSsr(async function ({req, res,}) {
+  const user = req.session.user;
+  const assessments = [
+    {
+      name: "Assessment 1",
+      module: "ACXXXXX - Module name",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis ex facilis alias veniam adipisci dicta numquam placeat, recusandae, quidem, excepturi temporibus a tempore architecto at? Necessitatibus eius laborum aspernatur quae!",
+      link: "/dashboard",
+      lecturer: "Jeff",
+      startDate: Date.now(),
+      submissionDeadline: Date.now(),
+      markingDeadline: Date.now(),
+      started: true,
+      closed: true,
+    },
+    {
+      name: "Assessment 2",
+      module: "ACXXXXX - Module name",
+      description: "Lorem ipsum dolor, eniam adipisci dicta numquam placeat, recusandae, quidem, excepturi temporibus a tempore architecto at? Necessitatibus eius laborum aspernatur quae!",
+      link: "/dashboard",
+      lecturer: "Jeff",
+      startDate: Date.now(),
+      submissionDeadline: Date.now(),
+      markingDeadline: Date.now(),
+      started: false,
+      closed: true,
+    },
+  ];
+
+  if (user === undefined) {
+    res.setHeader("location", "/");
+    res.statusCode = 302;
+
+    return {
+      props: {
+        user: { isLoggedIn: false },
+      },
+    };
+  }
+
+  return {
+    props: { user: req.session.user, assessments: assessments },
+  };
+}, sessionOptions);
