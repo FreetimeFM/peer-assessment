2691c680ac9c7768d8ea23817dc40f564252db41
FreetimeFM <awesomea380@tutanota.com>
2022-02-02 11:09:24 +0000

    Create assessment test data

--- components/AssessmentList.js
+++ components/AssessmentList.js
@@ -7,9 +7,38 @@ export default function AssessmentList({ assessments }) {
   assessments = [
     {
       name: "Assessment 1",
-      module: "ACXXXXX - Module name"
-    }
-  ]
+      module: "ACXXXXX - Module name",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis ex facilis alias veniam adipisci dicta numquam placeat, recusandae, quidem, excepturi temporibus a tempore architecto at? Necessitatibus eius laborum aspernatur quae!",
+      link: "/dashboard",
+      lecturer: "Jeff",
+      startDate: Date.now(),
+      submissionDeadline: Date.now(),
+      markingDeadline: Date.now(),
+      started: false
+    },
+    {
+      name: "Assessment 2",
+      module: "ACXXXXX - Module name",
+      description: "Lorem ipsum dolor, eniam adipisci dicta numquam placeat, recusandae, quidem, excepturi temporibus a tempore architecto at? Necessitatibus eius laborum aspernatur quae!",
+      link: "/dashboard",
+      lecturer: "Jeff",
+      startDate: Date.now(),
+      submissionDeadline: Date.now(),
+      markingDeadline: Date.now(),
+      started: false
+    },
+    {
+      name: "Assessment 3",
+      module: "ACXXXXX - Module name",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis ex facilis alias veniam adipisci dicta numquam placeat, recusandae, quidem, excepturi temporibus a tempore architecto at? Necessitatibus eius laborum aspernatur quae!",
+      link: "/dashboard",
+      lecturer: "Jeff",
+      startDate: Date.now(),
+      submissionDeadline: Date.now(),
+      markingDeadline: Date.now(),
+      started: true
+    },
+  ];
 
   return (
   )
