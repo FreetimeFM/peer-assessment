6de90ac54a91632bb91596d69c89f2d4820e8ffd
FreetimeFM <awesomea380@tutanota.com>
2022-02-02 11:19:47 +0000

    Create modal to show assessment details

--- components/AssessmentCard.js
+++ components/AssessmentCard.js
@@ -17,3 +17,63 @@ export default function AssessmentCard({ details }) {
     </Card>
   )
 }
+
+function InfoModal({trigger=<Button>Learn More</Button>, details}) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={trigger}
+    >
+      <Modal.Header>Assessment Details</Modal.Header>
+      <Modal.Content>
+        <Table celled>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell><strong>Name</strong></Table.Cell>
+              <Table.Cell>{details.name}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell><strong>Module</strong></Table.Cell>
+              <Table.Cell>{details.module}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell><strong>Lecturer</strong></Table.Cell>
+              <Table.Cell>{details.lecturer}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell><strong>Start Date</strong></Table.Cell>
+              <Table.Cell>{details.startDate}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell><strong>Submission Date</strong></Table.Cell>
+              <Table.Cell>{details.submissionDeadline}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell><strong>Marking Completion Date</strong></Table.Cell>
+              <Table.Cell>{details.markingDeadline}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell><strong>Description</strong></Table.Cell>
+              <Table.Cell>{details.description}</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button
+          content="Close"
+          onClick={() => setOpen(false)}
+        />
+        <Link href={details.link}>
+          <Button primary>
+            { details.started ? "Continue" : "Start" }
+          </Button>
+        </Link>
+      </Modal.Actions>
+    </Modal>
+  )
+}
