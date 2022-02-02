d8ccbaec392e1ebe100b7a13ef4b5161092d6b08
FreetimeFM <awesomea380@tutanota.com>
2022-02-02 13:03:31 +0000

    Make sidebar responsive

--- layouts/DashboardLayout.js
+++ layouts/DashboardLayout.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
+
 import { Sidebar } from "semantic-ui-react";
 
 import DashboardSidebar from "../components/DashboardSidebar";
 import PageHeader from "../components/PageHeader";
 
 export default function DashboardLayout({ children, username, pages, currentPage, onDashboardSidebarPageClick }) {
-
   const { name, subHeading, iconName } = pages[currentPage];
+  const [ device, setDevice ] = useState("desktop");
+
+  useEffect(() => {
+    window.addEventListener("resize", onWidthResize);
+  })
+
+  function onWidthResize(_e) {
+    let tempDevice = "desktop";
+
+    if (window.innerWidth < 576) tempDevice = "mobile";
+    else if (window.innerWidth < 768) tempDevice = "tablet";
+
+    if (tempDevice !== device) {
+      setDevice(tempDevice);
+    };
+  }
 
   return (
     <>
-      <DashboardSidebar username={username} pages={pages} currentPage={currentPage} isMobile={isMobile} onPageClick={onDashboardSidebarPageClick} />
+      <DashboardSidebar username={username} pages={pages} currentPage={currentPage} device={device} onPageClick={onDashboardSidebarPageClick} />
       <Sidebar.Pusher style={{ width: "calc(100% - 260px)", padding: "10px 20px" }}>
         <PageHeader heading={name} subHeading={subHeading} iconName={iconName} />
         <main>
