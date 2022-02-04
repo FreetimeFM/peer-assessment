a05907b2b6fb8f4eb297b2942b0643dbe473fac7
FreetimeFM <awesomea380@tutanota.com>
2022-02-04 16:36:37 +0000

    Store form data in state

--- components/CreateAssessment.js
+++ components/CreateAssessment.js
@@ -1,56 +1,87 @@
+import { useState } from "react";
 import { Form, Segment, Popup, Button } from "semantic-ui-react";
 
 export default function CreateAssessment() {
 
+  const [ formData, setFormData ] = useState({
+    name: "",
+    briefDescription: "",
+    description: "",
+    releaseDate: "",
+    submissionDeadline: "",
+    markingDeadline: ""
+  });
+  function updateForm(_e, { name, value }) {
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  function handleSubmit(e) {
+    console.log(formData);
+  }
+
   return (
     <Segment>
-      <Form>
+      <Form onSubmit={handleSubmit} >
         <Form.Group widths="equal">
-          <Form.Field required>
-            <label>Name <InputPopup message="The name of the assessment. 100 characters maximum. Required."/></label>
-            <input
-              name="name"
-              type="text"
-              placeholder="Required."
-              maxLength={100}
-              required
-            />
-          </Form.Field>
-        </Form.Group>
-        <Form.Field>
-          <label>Brief Description <InputPopup message="This description will be displayed as an overview in the dashboard. 500 characters maximum."/></label>
-          <textarea
-            name="briefDescription"
-            placeholder="Optional. This description will be displayed as an overview in the dashboard. 500 characters maximum."
-            maxLength={300}
-            rows={3}
+          <Form.Input
+            name="name"
+            label={<label>Name <InputPopup message="The name of the assessment. 100 characters maximum. Required."/></label>}
+            placeholder="Required."
+            maxLength={100}
+            onChange={updateForm}
+            required
           />
-        </Form.Field>
-        <Form.Field>
-          <label>Description <InputPopup message="A detailed description about the assessment. 5000 characters maximum."/></label>
-          <textarea
-            name="description"
-            placeholder="A detailed description about the assessment. 5000 characters maximum."
-            maxLength={5000}
-            rows={11}
+        </Form.Group>
+
+        <Form.TextArea
+          name="briefDescription"
+          label={<label>Brief Description <InputPopup message="This description will be displayed as an overview in the dashboard. 500 characters maximum."/></label>}
+          placeholder="Optional. This description will be displayed as an overview in the dashboard. 500 characters maximum."
+          maxLength={300}
+          rows={3}
+          onChange={updateForm}
+        />
+
+        <Form.TextArea
+          name="description"
+          label={<label>Description <InputPopup message="A detailed description about the assessment. 5000 characters maximum."/></label>}
+          placeholder="A detailed description about the assessment. 5000 characters maximum."
+          maxLength={5000}
+          rows={11}
+          onChange={updateForm}
+        />
+
+        <Form.Group widths="equal">
+          <Form.Input
+            name="releaseDate"
+            label={<label>Release Date <InputPopup message="The assessment will be accessible to students at this date. This is optional as you can manually release the assessment."/></label>}
+            type="datetime-local"
+            onChange={updateForm}
           />
-        </Form.Field>
-        <Form.Group widths="equal">
-          <Form.Field>
-            <label>Release Date <InputPopup message="The assessment will be accessible to students at this date. This is optional as you can manually release the assessment."/></label>
-            <input type="datetime-local" name="releaseDate"/>
-          </Form.Field>
         </Form.Group>
+
         <Form.Group widths="equal">
-          <Form.Field required>
-            <label>Submission Deadline <InputPopup message="The date students have to submit by. Required."/></label>
-            <input type="datetime-local" name="submissionDeadline" required/>
-          </Form.Field>
-          <Form.Field required>
-            <label>Marking Deadline <InputPopup message="The date students have to mark by. Required."/></label>
-            <input type="datetime-local" name="markingDeadline" required/>
-          </Form.Field>
+          <Form.Input
+            name="submissionDeadline"
+            label={<label>Submission Deadline <InputPopup message="The date students have to submit by. Required."/></label>}
+            type="datetime-local"
+            onChange={updateForm}
+            required
+          />
+
+          <Form.Input
+            name="markingDeadline"
+            label={<label>Marking Deadline <InputPopup message="The date students have to mark by. Required."/></label>}
+            type="datetime-local"
+            onChange={updateForm}
+            required
+          />
         </Form.Group>
+
+        <Form.Button content="Create" />
       </Form>
     </Segment>
   );
