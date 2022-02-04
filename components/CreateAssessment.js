43de3b199fa6190c2a236789a0133aa94d041075
FreetimeFM <awesomea380@tutanota.com>
2022-02-04 11:41:03 +0000

    Created blank component for creation form

---
+++ components/CreateAssessment.js
@@ -0,0 +1,8 @@
+
+
+export default function CreateAssessment({  }) {
+
+  return (
+    <h1>Create assessment</h1>
+  )
+}
--- pages/dashboard/create/assessment.js
+++ pages/dashboard/create/assessment.js
@@ -1,16 +1,17 @@
 import { withSessionSsr } from "../../../lib/iron-session/withSession";
 import DashboardLayout from "layouts/DashboardLayout";
+import CreateAssessment from "components/CreateAssessment";
 
 export default function createAssessment({ user }) {
 
   return (
     <DashboardLayout user={user}>
-
+      <CreateAssessment />
     </DashboardLayout>
   )
 }
 
-export const getServerSideProps = withSessionSsr(async function ({req, res}) {
+export const getServerSideProps = withSessionSsr(async function ({ req }) {
 
   // If the user is a student (unauthorised access), then send to login page.
   if (req.session.user.userType === 2) return {
