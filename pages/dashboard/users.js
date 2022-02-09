a50f290cb2f626ed1415f2c6009a1e70287b140f
FreetimeFM <awesomea380@tutanota.com>
2022-02-09 21:52:13 +0000

    Simplify get server side props method

--- pages/dashboard/users.js
+++ pages/dashboard/users.js
@@ -1,9 +1,24 @@
-import { withIronSessionSsr } from "iron-session/next";
 import DashboardLayout from "layouts/DashboardLayout";
-import { sessionOptions } from "lib/iron-session/session";
+import { withSessionSsr } from "lib/iron-session/withSession";
+
 export default function users({ user, result }) {
   return (
     <DashboardLayout user={user}>
     </DashboardLayout>
   )
 }
+
+export const getServerSideProps = withSessionSsr((req, res) => {
+
+  if (req.session.user.userType !== "admin") {
+    res.statusCode = 403;
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/dashboard"
+      }
+    };
+  }
+
+});
