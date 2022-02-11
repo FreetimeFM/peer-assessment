39d804a58489805190232887aa367b0cb40582f8
FreetimeFM <awesomea380@tutanota.com>
2022-02-11 15:54:56 +0000

    Create page for creating users

---
+++ pages/dashboard/create/user.js
@@ -0,0 +1,25 @@
+import DashboardLayout from "layouts/DashboardLayout";
+import { withSessionSsr } from "lib/iron-session/withSession";
+
+
+export default function ({ user }) {
+  return (
+    <DashboardLayout user={user}>
+
+    </DashboardLayout>
+  )
+}
+
+export const getServerSideProps = withSessionSsr(({ req, res }) => {
+
+  if (req.session.user.userType !== "admin") {
+    res.statusCode = 403;
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/dashboard"
+      }
+    };
+  }
+})
