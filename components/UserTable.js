42ddacfcf08c856dfab951bda7f25d8db382aa4a
FreetimeFM <awesomea380@tutanota.com>
2022-02-11 15:45:05 +0000

    Create mock user list

--- components/UserTable.js
+++ components/UserTable.js
@@ -1,5 +1,31 @@
   const [ fetching, setFetching ] = useState(false);
   const [ disableLoadMore, setDisableLoadMore ] = useState(true);
+  const [ userList, setUserList ] = useState([
+    {
+      refID: '323029331001475264',
+      name: 'Administrator',
+      email: 'admin@example.com',
+      userType: 'admin'
+    },
+    {
+      refID: '323037490948604096',
+      name: 'Brendan Cooper',
+      email: 'b.cooper@example.com',
+      userType: 'teacher'
+    },
+    {
+      refID: '323037568090243264',
+      name: 'Laura Sharp',
+      email: 'l.sharp@example.com',
+      userType: 'student'
+    },
+    {
+      refID: '323043769326764232',
+      name: 'Elijah Lynn',
+      email: 'e.lynn@example.com',
+      userType: 'student'
+    }
+  ]);
 
 export default function UserTable({ users }) {
 
