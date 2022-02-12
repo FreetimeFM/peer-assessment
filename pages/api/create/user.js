562a5c00272d3cd24492de60529068e326aab004
FreetimeFM <awesomea380@tutanota.com>
2022-02-12 16:48:32 +0000

    Use withSessionAPI

--- pages/api/create/user.js
+++ pages/api/create/user.js
@@ -1,5 +1,18 @@
+import { withSessionApi } from "lib/iron-session/withSession";
+import trim from "validator/lib/trim";
 
 
-export default function (req, res) {
+export default withSessionApi(async ({req, res}) => {
+  try {
+    let { name, email, userType } = req.body;
 
-}
+    // If the there is no data in the body or if the body is missing required data.
+    if (!req.body) return res.status(getHttpStatus(301)).json(createErrorPayload(301));
+    if (!name || !email || !userType) return res.status(getHttpStatus(301)).json(createErrorPayload(301));
+
+  } catch (error) {
+
+    // If something went wrong server side.
+    return res.status(getHttpStatus(300)).json(createErrorPayload(300));
+  }
+})
