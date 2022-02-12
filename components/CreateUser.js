f270eb537b41d86ee0bf73df017bac1cb49feb00
FreetimeFM <awesomea380@tutanota.com>
2022-02-12 14:23:06 +0000

    Added client side validation to form

--- components/CreateUser.js
+++ components/CreateUser.js
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import { Form } from "semantic-ui-react";
+import Joi from "joi";
 
 import FormInputPopup from "./FormInputPopup";
 
 export default function CreateUser() {
 
+  const userTypeOptions = [
+    { key: 1, text: "Student", value: "student" },
+    { key: 2, text: "Teacher", value: "teacher" },
+    { key: 3, text: "Administrator", value: "admin" },
+  ]
   const [ formData, setFormData ] = useState({
     name: "",
     email: "",
-    userType: ""
+    userType: "student"
   });
-  const [ formError, setFormError ] = useState({
-    name: "",
-    email: "",
-    userType: ""
-  });
+  const [ formError, setFormError ] = useState({});
+  const [ formCheck, setFormCheck ] = useState(false);
 
   function handleChange(e, { name, value }) {
     setFormData({
@@ -23,24 +26,75 @@ export default function CreateUser() {
     })
   }
 
-  function handleSubmit(e) {
+  // Uses joi to validate the email address and password.
+  async function validate() {
+
+    const nameCheck = Joi.string()
+    .required()
+    .trim()
+    .max(50)
+    .messages({
+      "string.empty": "Cannot be empty",
+      "string.max": "Too long",
+    })
+    .validate(formData.name);
+
+    if (nameCheck.error) {
+      setFormError({
+        ...formError,
+        name: nameCheck.error.details[0].message
+      })
+      return false;
+    }
+
+    const emailCheck = Joi.string()
+    .required() // Checks if field is empty.
+    .trim() // Removes leading and trailing whitespace.
+    .max(500) // Checks if its too long (500 chars).
+    .email({tlds: false}) // Checks email.
+    .messages({ // Various error messages according to which test it failed.
+      "string.empty": "Cannot be empty",
+      "string.max": "Too long",
+      "string.email": "Invalid email address",
+    })
+    .validate(formData.email);
+
+    if (emailCheck.error) {
+      setFormError({
+        ...formError,
+        email: emailCheck.error.details[0].message
+      })
+      return false;
+    }
+
+    return true;
+  }
+
+  async function handleSubmit(e) {
     e.preventDefault();
+    setFormCheck(true);
+    setFormError({});
     console.log(formData);
 
+    if (!await validate()) {
+      setFormCheck(false);
+      return;
+    }
+
+    setFormCheck(false);
   }
 
   return (
-    <Form>
+    <Form loading={formCheck}>
       <Form.Group widths="equal">
         <Form.Input
           name="name"
           icon="user"
           iconPosition="left"
-          placeholder="Joe Bloggs"
           maxLength={50}
           onChange={handleChange}
           label={<label>Name <FormInputPopup message="The full name of the user you want to add. Maximum length is 50 characters. Required."/></label>}
-          error={formError.name === "" ? false : { content: formError.name }}
+          error={formError.name ? { content: formError.name } : false}
           required
           fluid
         />
@@ -49,11 +103,10 @@ export default function CreateUser() {
           type="email"
           icon="mail"
           iconPosition="left"
-          placeholder="name@example.com"
           maxLength={500}
           onChange={handleChange}
           label={<label>Email Address <FormInputPopup message="The email address is a unique identifier. No other user can have the same email address. Maximum length is 500 characters. Required."/></label>}
-          error={formError.email === "" ? false : { content: formError.email }}
+          error={formError.email ? { content: formError.email } : false}
           required
           fluid
         />
@@ -62,20 +115,14 @@ export default function CreateUser() {
         <Form.Select
           name="userType"
           label={<label>Role <FormInputPopup message="The role of the user. Required."/></label>}
-          options={[
-            { key: 1, text: "Student", value: "student" },
-            { key: 2, text: "Teacher", value: "teacher" },
-            { key: 3, text: "Administrator", value: "admin" },
-          ]}
+          options={userTypeOptions}
           defaultValue="student"
           onChange={handleChange}
-          error={formError.userType === "" ? false : { content: formError.userType }}
           required
         />
       </Form.Group>
       <Form.Button
         content="Submit"
-        size="large"
         onClick={handleSubmit}
         primary
         fluid
