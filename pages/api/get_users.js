28f301e1417fa5d0751b2265bb4101896a62dd25
FreetimeFM <awesomea380@tutanota.com>
2022-02-14 11:10:19 +0000

    Improved get_users api robustness

--- pages/api/get_users.js
+++ pages/api/get_users.js
@@ -4,23 +4,22 @@ import { getUsers } from "lib/database";
 import isInt from "validator/lib/isInt";
 
 export default withSessionApi(async ({ req, res }) => {
+  try {
+    if (req.session.user.userType !== "admin") return res.status(getHttpStatus(303)).json(createErrorPayload(303));
 
-  if (req.session.user.userType !== "admin") return res.status(getHttpStatus(303)).json(createErrorPayload(303));
+    const { size, afterRefID } = req.body;
 
-  let { size, afterRefID } = req.body;
+    if (size) {
+      if (!isInt(size.toString())) size = undefined;
+    }
 
-  if (size) {
-    if (!isInt(size.toString())) size = undefined;
+    if (afterRefID) {
+      if (!isInt(afterRefID)) afterRefID = undefined;
+    }
+
+    res.status(200).json({ ...await getUsers(size, afterRefID) });
+
+  } catch (error) {
+    res.status(getHttpStatus(300)),json(createErrorPayload(300));
   }
-
-  if (afterRefID) {
-    if (!isInt(afterRefID)) afterRefID = undefined;
-  }
-
-  const queryResult = await getUsers(size, afterRefID);
-
-  console.log(queryResult);
-
-  res.status(200).json({ error: false, result: queryResult });
-
 })
