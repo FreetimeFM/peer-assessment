8aafe65724d820b3eedae0c4d1d7e463e1d5ed9f
FreetimeFM <awesomea380@tutanota.com>
2022-02-14 15:40:58 +0000

    Added input popup labels

--- components/CreateClass.js
+++ components/CreateClass.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import fetchJson from "lib/iron-session/fetchJson";
 import { Form, Message } from "semantic-ui-react";
-
+import FormInputPopup from "./FormInputPopup";
 
 export default function CreateClass({ user }) {
 
+  const [ studentsDropdown, setStudentsDropdown ] = useState([]);
+  const [ teachersDropdown, setTeachersDropdown ] = useState([]);
+
+  async function fetchStudentsAndTeachers() {
+
+    try {
+
+      const response = await fetchJson("/api/")
+
+    } catch (error) {
+
+    }
+
+  }
 
   return (
     <Form>
@@ -10,7 +26,7 @@ export default function CreateClass({ user }) {
         <Form.Input
           name="name"
           icon="user"
-          label="Class name"
+          label={<label>Class name <FormInputPopup message="The name of the class that will be shown to students and lecturers. Required." /></label>}
           iconPosition="left"
           placeholder="Required."
           maxLength={70}
@@ -20,23 +36,25 @@ export default function CreateClass({ user }) {
       </Form.Group>
       <Form.Group widths="equal">
         <Form.Dropdown
-          label="Select co-teachers"
-          options={[{ key: 1, text: "Teacher 1 (email)", value: "ref 1" }, { key: 2, text: "Teacher 2 (email)", value: "ref 2" }]}
+          label={<label>Teacher(s) <FormInputPopup message="Select the teachers who will have access to this class." /></label>}
+          options={teachersDropdown}
           fluid
           multiple
           search
           selection
+          required
         />
       </Form.Group>
       <Message content="You are automatically included as a teacher. As a result, your name will not be shown in the list." hidden={user.userType === "admin"} info/>
       <Form.Group widths="equal">
         <Form.Dropdown
-          label="Choose students"
-          options={[{ key: 1, text: "Student 1 (email)", value: "ref 1" }, { key: 2, text: "Student 2 (email)", value: "ref 2" }]}
+          label={<label>Student(s) <FormInputPopup message="Select the students who will have access to this class." /></label>}
+          options={studentsDropdown}
           fluid
           multiple
           search
           selection
+          required
         />
       </Form.Group>
       <Form.Button content="Submit" primary/>
