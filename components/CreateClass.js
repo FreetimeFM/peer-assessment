45445c3e4390ce5cdb2e138bd68cbd8f6069e07b
FreetimeFM <awesomea380@tutanota.com>
2022-02-14 17:08:26 +0000

    Fetch users from API and put results in dropdown

--- components/CreateClass.js
+++ components/CreateClass.js
@@ -2,22 +2,78 @@ import { useState } from "react";
 import fetchJson from "lib/iron-session/fetchJson";
 import { Form, Message } from "semantic-ui-react";
 import FormInputPopup from "./FormInputPopup";
+import { useEffect } from "react";
 
 export default function CreateClass({ user }) {
 
   const [ studentsDropdown, setStudentsDropdown ] = useState([]);
   const [ teachersDropdown, setTeachersDropdown ] = useState([]);
+  const [ fetchedUsers, setFetchedUsers ] = useState(false);
+  const [ error, setError ] = useState("");
+
+  useEffect(() => {
+    fetchStudentsAndTeachers();
+  })
 
   async function fetchStudentsAndTeachers() {
+    if (fetchedUsers) return;
+    let response;
 
     try {
+      response = await fetchJson("/api/get_users_by_usertype", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ userType: "teacher" })
+      })
 
-      const response = await fetchJson("/api/")
+      if (response.error) {
+        console.error("Error fetching teachers:", response);
+        setError("There has been an error fetching teachers. Please contact your administrator and check the console/logs.");
+        return;
+      }
+
+      handleResponseData(response.result, true);
+
+      response = await fetchJson("/api/get_users_by_usertype", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ userType: "student" })
+      })
+
+      if (response.error) {
+        console.error("Error fetching students:", response);
+        setError("There has been an error fetching students. Please contact your administrator and check the console/logs.");
+        return;
+      }
+
+      handleResponseData(response.result);
 
     } catch (error) {
-
+      console.error("response", response);
+      console.error("error", error);
+      setError("An unknown error has occured. Please contact your administrator and check the console/logs.");
     }
 
+    setFetchedUsers(true);
+  }
+
+  function handleResponseData(data, teacher = false) {
+
+    const options = data.map((value, index) => {
+      return {
+        key: index,
+        text: `${value.name} (${value.email})`,
+        value: value.refID
+      }
+    });
+
+    teacher ? setTeachersDropdown(options) : setStudentsDropdown(options);
   }
 
   return (
