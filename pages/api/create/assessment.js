1e2e903f9bfa24570f92f57f53c76cbe546a9be1
FreetimeFM <awesomea380@tutanota.com>
2022-02-14 15:22:13 +0000

    Removed files and modules not related to assessments

--- pages/api/create/assessment.js
+++
@@ -1,9 +0,0 @@
-import { withSessionApi } from "lib/iron-session/withSession";
-import createAssessment from "pages/dashboard/create/assessment";
-
-
-export default withSessionApi(async function ({ req, res }) {
-
-
-
-})
