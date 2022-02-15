7d93590248e25ee523ffcc0c6618b57bbb65ce24
FreetimeFM <awesomea380@tutanota.com>
2022-02-15 11:47:13 +0000

    Created table layout

--- components/ClassTable.js
+++ components/ClassTable.js
@@ -1,10 +1,30 @@
-import { Table } from "semantic-ui-react";
+import { useState } from "react";
+import { Table, Message } from "semantic-ui-react";
+import Placeholder from "./Placeholder";
 
 
 export default function ClassTable({ user }) {
-  return (
-    <Table>
 
-    </Table>
+  if (user.userType !== "admin") return <Placeholder iconName="time" message="Not available." extraContent="This feature hasn't been implemented yet." />
+
+  const [ classList, setClassList ] = useState([]);
+
+  return (
+    <>
+      <Message error/>
+      <Message content="Fetching users. Please wait." info/>
+      <Table attached="bottom" celled selectable striped>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell content="Name" />
+            <Table.HeaderCell content="Teachers" />
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+
+        </Table.Body>
+      </Table>
+    </>
   )
 }
