0ee8262545a3e11bed172544c6d6cf3bead84b22
FreetimeFM <awesomea380@tutanota.com>
2022-02-15 10:59:08 +0000

    Use query to create class

--- pages/api/create/class.js
+++ pages/api/create/class.js
@@ -1,18 +1,21 @@
+import { createClass } from "lib/database";
 import { createErrorPayload, getHttpStatus } from "lib/errors";
 import { withSessionApi } from "lib/iron-session/withSession";
 
-
-export default withSessionApi(({req, res}) => {
+export default withSessionApi(async ({req, res}) => {
 
   try {
     if (req.session.user.userType !== "admin") return res.status(getHttpStatus(303)).json(createErrorPayload(303));
 
     const { name, teachers, students } = req.body;
 
-    console.log(req.body);
+    if (!name || !teachers || !students) return res.status(getHttpStatus(301)).json(createErrorPayload(301));
+
+    await createClass(name, teachers, students);
 
     res.status(200).json({error: false});
   } catch (error) {
+    console.log(error);
     res.status(getHttpStatus(300)).json(createErrorPayload(300));
   }
 })
