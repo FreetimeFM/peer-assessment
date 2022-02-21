1c77dba4067522c1e7decc987bbb613ba706f42f
FreetimeFM <awesomea380@tutanota.com>
2022-02-21 11:40:02 +0000

    Wrapped api code in trycatch block

--- pages/api/create/assessment.js
+++ pages/api/create/assessment.js
@@ -1,17 +1,21 @@
 import { withSessionApi } from "lib/iron-session/withSession";
-import { createErrorPayload } from "lib/errors";
+import { createErrorPayload, getHttpStatus } from "lib/errors";
 import { createAssessment } from "lib/database";
 
 export default withSessionApi(async function (req, res) {
 
-  if (!req.body) return res.status(400).json(createErrorPayload(301));
+  try {
+    if (!req.body) return res.status(400).json(createErrorPayload(301));
 
-  const db = await createAssessment({
-    ...req.body,
-    lecturerRef: req.session.user.ref
-  })
+    await createAssessment({
+      ...req.body,
+      lecturerRef: req.session.user.ref
+    })
 
-  return {
-    error: false
-  };
+    return res.status(200).json({
+      error: false
+    });
+  } catch (error) {
+    return res.status(getHttpStatus(300)).json(createErrorPayload(300));
+  }
 })
