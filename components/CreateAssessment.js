84c3055440d7a3002f0fa3c14367d7779c0ead7d
FreetimeFM <awesomea380@tutanota.com>
2022-02-23 11:09:06 +0000

    Moved questionTypes to its own file

--- components/CreateAssessment.js
+++ components/CreateAssessment.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Form, Card, Header } from "semantic-ui-react";
+import { Form, Card } from "semantic-ui-react";
 
 import fetchJson from "../lib/iron-session/fetchJson";
 import FormInputPopup from "./FormInputPopup";
+import { questionTypes } from "lib/questionTypes";
 
 export default function CreateAssessment({ userRef }) {
 
@@ -150,49 +151,6 @@ function StageOne({ onSubmit }) {
 function StageTwo({ onReverseStage, onSubmit }) {
 
   const [ questions, setQuestions ] = useState([]);
-  const questionTypes = [{
-    key: 0,
-    text: "Short Text",
-    value: "short-text",
-    content: (
-      <Header icon="font" content="Short text" subheader="Students answer using text limited to a single line. Maximum 500 characters." />
-    )
-  },
-  {
-    key: 1,
-    text: "Long Text",
-    value: "long-text",
-    content: (
-      <Header icon="list" content="Long text" subheader="Students answer using text that can span multiple lines. Maximum 5000 characters." />
-    )
-  },
-  {
-    key: 2,
-    text: "Single Selection",
-    value: "dropdown",
-    disabled: true,
-    content: (
-      <Header icon="caret square down" content="Single Selection" subheader="Students choose one answer out of many options." />
-    )
-  },
-  {
-    key: 3,
-    text: "Multiple Selection",
-    value: "multi-select",
-    disabled: true,
-    content: (
-      <Header icon="check square" content="Multiple Selection" subheader="Students choose one or more answers out of one or many options." />
-    )
-  },
-  {
-    key: 4,
-    text: "More Options",
-    value: "more",
-    disabled: true,
-    content: (
-      <Header icon="hourglass" content="More Options" subheader="More question types will be implemented soon." />
-    )
-  }];
 
   function handleAddQuestion(newQuestion) {
     setQuestions([ ...questions, newQuestion ]);
@@ -216,9 +174,8 @@ function StageTwo({ onReverseStage, onSubmit }) {
   return (
     <Form onSubmit={onSubmit} >
 
-      <DisplayQuestions questions={questions} questionTypes={questionTypes} />
+      <DisplayQuestions questions={questions} />
       <CreateQuestion
-        questionTypes={questionTypes}
         onAddQuestion={handleAddQuestion}
         onRemoveAll={handleRemoveAll}
       />
@@ -245,7 +202,7 @@ function DisplayQuestions({ questions }) {
   })
 }
 
-function CreateQuestion({ questionTypes, onAddQuestion, onRemoveAll }) {
+function CreateQuestion({ onAddQuestion, onRemoveAll }) {
 
   const [ qName, setQName ] = useState("");
   const [ qType, setQType ] = useState(questionTypes[0].value);
