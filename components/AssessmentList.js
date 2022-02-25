b210331553af9d051541d200e0381c00764e6320
FreetimeFM <awesomea380@tutanota.com>
2022-02-25 14:08:09 +0000

    Fetch assessments from api

--- components/AssessmentList.js
+++ components/AssessmentList.js
@@ -1,12 +1,62 @@
+import { useState, useEffect } from "react";
 import { Card, Button } from "semantic-ui-react";
 import Link from "next/link";
 
 import AssessmentCard from "./AssessmentCard";
-import Placeholder from "./Placeholder";
+import Placeholder, { placeholderTemplate } from "./Placeholder";
+import fetchJson from "lib/iron-session/fetchJson";
 
-export default function AssessmentList({ assessments, userType = 2, past = false }) {
+export default function AssessmentList({ userType = 2, past = false }) {
 
-  if (!assessments) return (
+  if (past) return placeholderTemplate();
+
+  const [ assessmentList, setAssessmentList] = useState();
+  const [ fetchOptions, setFetchOptions ] = useState({
+    fetched: false,
+    fetching: true
+  });
+
+  useEffect(() => {
+    fetchAssessments()
+  }, [])
+
+  async function fetchAssessments() {
+    if (fetchOptions.fetched) return;
+    setFetchOptions({
+      fetched: false,
+      fetching: true
+    });
+
+    try {
+      const response = await fetchJson("/api/get_assessments_overview", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      console.log(response);
+
+      if (response.error) {
+        console.error(response);
+      }
+      else setAssessmentList(response.result);
+
+    } catch (error) {
+      console.error(error);
+    }
+
+    setFetchOptions({ fetched: true, fetching: false });
+  }
+
+  if (fetchOptions.fetching) return (
+    <Placeholder
+      message={`We're fetching your ${past ? "past" : ""} assessments.`}
+      iconName="cloud download"
+      extraContent={<p>Please wait.</p>}
+    />
+  )
+
+  if (!assessmentList) return (
     <Placeholder
       message={`We're having trouble fetching your ${past ? "past" : ""} assessments.`}
       iconName="close"
@@ -14,9 +64,9 @@ export default function AssessmentList({ assessments, userType = 2, past = false
     />
   )
 
-  if (assessments.length > 0) return (
+  if (assessmentList.length > 0) return (
     <Card.Group>
-      {assessments.map((assessment, index) => {
+      {assessmentList.map((assessment, index) => {
         return (
           <AssessmentCard
             key={index}
