3861fa5627097c34e8aae204e58a7a54c63a52b8
FreetimeFM <awesomea380@tutanota.com>
2022-02-27 12:48:23 +0000

    Created component to display assessment questions

---
+++ components/DisplayAssessment.js
@@ -0,0 +1,12 @@
+import { Form } from "semantic-ui-react";
+
+
+export default function DisplayAssessment({ user, assessmentRefID }) {
+
+
+  return (
+    <Form>
+
+    </Form>
+  )
+}
--- pages/dashboard/assess/[assessmentRefID].js
+++ pages/dashboard/assess/[assessmentRefID].js
@@ -1,13 +1,15 @@
 import { useRouter } from "next/router"
 
 import { withSessionSsr } from "lib/iron-session/withSession"
+import DisplayAssessment from "components/DisplayAssessment";
 
 export default function ({ user }) {
   const assessmentRefID = useRouter().query.assessmentRefID
 
   return (
     <>
-      <h1>{assessmentRefID}</h1>
+
+      <DisplayAssessment user={user} assessmentRefID={assessmentRefID} />
     </>
   )
 }
