0fd83452a462da5a48ffdae2ea29ae68a90ab209
FreetimeFM <awesomea380@tutanota.com>
2022-02-27 12:23:14 +0000

    Include db call to get classes for teachers

--- pages/api/get_classes.js
+++ pages/api/get_classes.js
@@ -1,15 +1,24 @@
-import { getAllClasses } from "lib/database";
+import { getAllClasses, getClassesByTeacherRefID } from "lib/database";
 import { createErrorPayload, getHttpStatus } from "lib/errors";
 import { withSessionApi } from "lib/iron-session/withSession";
 
 export default withSessionApi(async ({req, res}) => {
   try {
 
-    if (req.session.user.userType !== "admin") return res.status(getHttpStatus(303)).json(createErrorPayload(303));
+    switch (req.session.user.userType) {
+      case "admin":
+        return res.status(200).json({
+          ...await getAllClasses()
+        });
 
-    return res.status(200).json({
-      ...await getAllClasses()
-    })
+      case "teacher":
+        return res.status(200).json({
+          ...await getClassesByTeacherRefID(req.session.user.refID)
+        });
+
+      default:
+        return res.status(getHttpStatus(303)).json(createErrorPayload(303));
+    }
   } catch (error) {
     return res.status(getHttpStatus(300)).json(createErrorPayload(300));
   }
