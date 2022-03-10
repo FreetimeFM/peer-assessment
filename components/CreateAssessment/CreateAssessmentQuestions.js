cd38b92dd53352adab6a25159d688ea059967cee
FreetimeFM <awesomea380@tutanota.com>
2022-03-10 15:10:55 +0000

    Use modal instead of card to add questions

--- components/CreateAssessment/CreateAssessmentQuestions.js
+++ components/CreateAssessment/CreateAssessmentQuestions.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, Card, Button } from "semantic-ui-react";
+import { Form, Card, Button, Modal } from "semantic-ui-react";
 
 import FormInputPopup from "components/FormInputPopup";
 import { QuestionField } from "components/QuestionCard";
@@ -15,7 +15,7 @@ export function CreateAssessmentQuestions({ questions, onAddQuestion, onRemoveQu
         onRemove={onRemoveQuestion}
       />
       <CreateAssessmentQuestion
-        onAdd={onAddQuestion}
+        onAddQuestion={onAddQuestion}
         onRemove={onRemoveQuestion}
       />
     </>
@@ -52,80 +52,94 @@ function DisplayAssessmentQuestions({ questions, onRemove }) {
   })
 }
 
-function CreateAssessmentQuestion({ onAdd }) {
-
+function CreateAssessmentQuestion({ onAddQuestion }) {
   const qTypes = getDropdownOptions(questionTypes);
-
-  const [ question, setQuestion ] = useState({
+  const defaultQuestionData = {
     name: "",
     marks: 1,
     type: qTypes[0].value
-  });
+  }
 
-  function handleClick(_e) {
+  const [ open, setOpen ] = useState(false);
+  const [ question, setQuestion ] = useState(defaultQuestionData);
+
+  function onAdd(_e) {
     if (question.name.length === 0) return;
-    onAdd(question);
-    setQuestion({
-      name: "",
-      marks: 1,
-      type: qTypes[0].value
-    });
+    onAddQuestion(question);
+    setOpen(false);
+    setQuestion(defaultQuestionData);
+  }
+
+  function onCancel(_e) {
+    setOpen(false);
+    setQuestion(defaultQuestionData);
   }
 
   return (
-    <Card color="blue" fluid raised>
-      <Card.Content content={<Card.Header content="Create an assessment question" />} />
-      <Card.Content>
-        <Form.Group>
+    <Modal
+      open={open}
+      trigger={
+        <Form.Button
+          content="Add assessment question"
+          onClick={_e => setOpen(true)}
+          primary
+          fluid
+        />
+      }
+    >
+      <Modal.Header content="Create an assessment question." />
+      <Modal.Content>
+        <Form>
           <Form.Input
             name="name"
             label={<label>Assessment Question <FormInputPopup message="The question you want to assess. 150 characters maximum. Required."/></label>}
             placeholder="Required."
             value={question.name}
             onChange={(_e, {value}) => {
-              setQuestion({ ...question, "name": value })
+              setQuestion({ ...question, "name": value });
             }}
             maxLength={150}
-            width="8"
             required
           />
-          <Form.Input
-            name="marks"
-            type="number"
-            label={<label>Marks <FormInputPopup message="The number of marks this question is worth. Required."/></label>}
-            placeholder="Required."
-            value={question.marks}
-            onChange={(_e, {value}) => {
-              setQuestion({ ...question, "marks": value })
-            }}
-            min={0}
-            max={1000}
-            width="3"
-            required
-          />
-          <Form.Dropdown
-            name="type"
-            label={<label>Type <FormInputPopup message="The type of question. Required."/></label>}
-            options={qTypes}
-            value={question.type}
-            onChange={(_e, {value}) => {
-              setQuestion({ ...question, "type": value })
-            }}
-            width="5"
-            selection
-            required
-          />
-        </Form.Group>
-      </Card.Content>
-      <Card.Content extra>
-        <Form.Button
-          type="submit"
-          content="Add Question"
-          onClick={handleClick}
+          <Form.Group>
+            <Form.Input
+              name="marks"
+              type="number"
+              label={<label>Marks <FormInputPopup message="The number of marks this question is worth. Required."/></label>}
+              placeholder="Required."
+              value={question.marks}
+              onChange={(_e, {value}) => {
+                setQuestion({ ...question, "marks": value })
+              }}
+              min={0}
+              max={1000}
+              width="4"
+              required
+            />
+            <Form.Dropdown
+              name="type"
+              label={<label>Type <FormInputPopup message="The type of question. Required."/></label>}
+              options={qTypes}
+              value={question.type}
+              onChange={(_e, {value}) => {
+                setQuestion({ ...question, "type": value })
+              }}
+              width="12"
+              selection
+              required
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button content="Cancel" onClick={onCancel} />
+        <Button
+          content="Add"
+          onClick={onAdd}
           disabled={question.name.length === 0}
           primary
         />
-      </Card.Content>
-    </Card>
-  );
+      </Modal.Actions>
+    </Modal>
+  )
 }
