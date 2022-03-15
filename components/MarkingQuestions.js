56c1f795d1905a0393d7ca1193ed311f56ba3fa4
FreetimeFM <awesomea380@tutanota.com>
2022-03-15 15:22:13 +0000

    Display questions and marking criteria

--- components/MarkingQuestions.js
+++ components/MarkingQuestions.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Form } from "semantic-ui-react";
-import QuestionCard from "./QuestionCard";
+import { Card, Form } from "semantic-ui-react";
+import FormInputPopup from "./FormInputPopup";
+import { QuestionField } from "./QuestionCard";
 
-export default function AssessmentQuestions({ questions, onSubmit, preview = false, errorList = [] }) {
+export default function ({ data, onSubmit, preview = false, errorList = [] }) {
   const [ answers, setAnswers ] = useState({});
 
   function handleAnswerInput(_e, {name, value}) {
@@ -15,29 +16,50 @@ export default function AssessmentQuestions({ questions, onSubmit, preview = fal
   return (
     <Form>
       {
-        questions.map((item, index) => {
+        data.questions.map((item, index) => {
           return (
-            <QuestionCard
-              index={index}
-              question={item}
-              preview={preview}
-              onAnswerInput={handleAnswerInput}
-            />
+            <Card
+              key={index}
+              fluid
+            >
+              <Card.Content
+                header={`${index + 1}. ${item.name}`}
+                meta={`${item.marks} ${item.marks === 1 ? "mark" : "marks"}`}
+              />
+              <Card.Content>
+                Student Answer
+              </Card.Content>
+              <Card.Content>
+                {renderMarkingQuestions(data.markingCriteria.questions[index])}
+                <Form.Input
+                  name="marks"
+                  type="number"
+                  label={<label>Allocate Marks <FormInputPopup message="How many marks is this answer worth? Required."/></label>}
+                  placeholder="Required."
+                  min={0}
+                  max={item.marks}
+                  readOnly={preview}
+                  required
+                />
+              </Card.Content>
+            </Card>
           )
         })
       }
-      <Form.Button
-        type="submit"
-        style={{ display: "none" }}
-        disabled
-      />
-      <Form.Button
-        content="Submit"
-        onClick={_e => onSubmit(answers)}
-        disabled={preview}
-        primary
-        fluid
-      />
     </Form>
   )
+}
+
+function renderMarkingQuestions(questions, preview = false) {
+
+  if (questions) return questions.map((item, index) => {
+    return (
+      <QuestionField
+        label={`${index + 1}. ${item.name}`}
+        type={item.type}
+        preview={preview}
+        marking
+      />
+    )
+  })
 }
