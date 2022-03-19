6f924815ce92e3f95edb9d40511663172b90f8ed
FreetimeFM <awesomea380@tutanota.com>
2022-03-19 15:41:04 +0000

    Added more templates for placeholder

--- components/Placeholder.js
+++ components/Placeholder.js
@@ -12,15 +12,31 @@ export default function PlaceHolder({ message, iconName, extraContent = null }) 
   )
 }
 
-export function placeholderTemplate(type) {
-
+export function placeholderTemplate(type, message, extraContent) {
   switch (type) {
+    case "fetch":
+      return (
+        <PlaceHolder
+          iconName="cloud download"
+          message={message ? message : "Fetching details"}
+          extraContent={extraContent ? extraContent : "Please wait while we fetch your details."}
+        />
+      )
+
+    case "error":
+      return (
+        <PlaceHolder
+          iconName="exclamation"
+          message={message ? message : "Error"}
+          extraContent={extraContent ? extraContent : "Something went wrong. Please contact your adminstrator."}
+        />
+      )
 
     default:
       return (
         <PlaceHolder
-          message="Not Implemented."
           iconName="clock"
+          message="Not Implemented."
           extraContent={<p>This feature hasn't been implemented yet.</p>}
         />
       )
