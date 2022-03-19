346e6d313742691a4558e8344149a45dde66e414
FreetimeFM <awesomea380@tutanota.com>
2022-03-19 11:29:37 +0000

    Moved assessments overview fetching to assessments page

--- components/AssessmentList.js
+++ components/AssessmentList.js
@@ -1,72 +1,16 @@
-import { useState, useEffect } from "react";
 import { Card, Button } from "semantic-ui-react";
 import Link from "next/link";
 
 import AssessmentCard from "./AssessmentCard";
 import PlaceHolder, { placeholderTemplate } from "./PlaceHolder";
-import fetchJson from "lib/iron-session/fetchJson";
 
-export default function AssessmentList({ userType = 2, past = false }) {
+export default function AssessmentList({ assessments, userType = 2, past = false }) {
 
   if (past) return placeholderTemplate();
 
-  const [ assessmentList, setAssessmentList] = useState();
-  const [ fetchOptions, setFetchOptions ] = useState({
-    fetched: false,
-    fetching: true
-  });
-
-  useEffect(() => {
-    fetchAssessments()
-  }, [])
-
-  async function fetchAssessments() {
-    if (fetchOptions.fetched) return;
-    setFetchOptions({
-      fetched: false,
-      fetching: true
-    });
-
-    try {
-      const response = await fetchJson("/api/get_assessments_overview", {
-        headers: {
-          Accept: "application/json",
-        },
-      });
-
-      console.log(response);
-
-      if (response.error) {
-        console.error(response);
-      }
-      else setAssessmentList(response.result);
-
-    } catch (error) {
-      console.error(error);
-    }
-
-    setFetchOptions({ fetched: true, fetching: false });
-  }
-
-  if (fetchOptions.fetching) return (
-    <PlaceHolder
-      message={`We're fetching your ${past ? "past" : ""} assessments.`}
-      iconName="cloud download"
-      extraContent={<p>Please wait.</p>}
-    />
-  )
-
-  if (!assessmentList) return (
-    <PlaceHolder
-      message={`We're having trouble fetching your ${past ? "past" : ""} assessments.`}
-      iconName="close"
-      extraContent={<p>Please contact your administrator.</p>}
-    />
-  )
-
-  if (assessmentList.length > 0) return (
+  if (assessments.length > 0) return (
     <Card.Group>
-      {assessmentList.map((assessment, index) => {
+      {assessments.map((assessment, index) => {
         return (
           <AssessmentCard
             key={index}
--- pages/dashboard/assessments.js
+++ pages/dashboard/assessments.js
@@ -1,15 +1,71 @@
+import { useState, useEffect } from "react";
+
 import { withSessionSsr } from "../../lib/iron-session/withSession";
+import fetchJson from "lib/iron-session/fetchJson";
 import DashboardLayout from "../../layouts/DashboardLayout";
 import AssessmentList from "../../components/AssessmentList";
 
-export default function CurrentAssessments({ user, result }) {
+export default function CurrentAssessments({ user }) {
+
+  const [ assessmentList, setAssessmentList] = useState();
+  const [ fetchOptions, setFetchOptions ] = useState({
+    fetched: false,
+    fetching: true
+  });
+
+  useEffect(() => {
+    fetchAssessments()
+  }, [])
+
+  async function fetchAssessments() {
+    if (fetchOptions.fetched) return;
+    setFetchOptions({
+      fetched: false,
+      fetching: true
+    });
+
+    try {
+      const response = await fetchJson("/api/get_assessments_overview", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      console.log(response);
+
+      if (response.error) {
+        console.error(response);
+      }
+      else setAssessmentList(response.result);
+
+    } catch (error) {
+      console.error(error);
+    }
+
+    setFetchOptions({ fetched: true, fetching: false });
+  }
+
+  if (fetchOptions.fetching) return (
+    <PlaceHolder
+      message={`We're fetching your ${past ? "past" : ""} assessments.`}
+      iconName="cloud download"
+      extraContent={<p>Please wait.</p>}
+    />
+  )
+
+  if (!assessmentList) return (
+    <PlaceHolder
+      message={`We're having trouble fetching your ${past ? "past" : ""} assessments.`}
+      iconName="close"
+      extraContent={<p>Please contact your administrator.</p>}
+    />
+  )
 
   return (
     <DashboardLayout user={user}>
-      <AssessmentList userType={user.userType} />
+      <AssessmentList userType={user.userType} assessments={assessmentList} />
     </DashboardLayout>
   )
-
 }
 
 export const getServerSideProps = withSessionSsr()
