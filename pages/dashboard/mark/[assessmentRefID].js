acdbdaeb721636085cf5cc1adef8750582e4114d
FreetimeFM <awesomea380@tutanota.com>
2022-03-24 13:54:13 +0000

    Place all assessment content in padded container

--- pages/dashboard/mark/[assessmentRefID].js
+++ pages/dashboard/mark/[assessmentRefID].js
@@ -10,7 +10,7 @@ import Metadata from "components/Metadata";
 import MarkingQuestions, { GeneralMarkingQuestions } from "components/MarkingQuestions";
 import Link from "next/link";
 
-export default function () {
+export default function ({ user }) {
 
   const assessmentRefID = useRouter().query.assessmentRefID;
   const [ peerIndex, setPeerIndex ] = useState(0);
@@ -115,7 +115,6 @@ export default function () {
 
       } else {
 
-
         if (peerIndex === peersToMark.length - 1) {
           setCompleted(true);
           alert("Your feedback has been successfully submitted.")
@@ -152,154 +151,142 @@ export default function () {
     return marks;
   }
 
-  if (fetchOptions.fetching) return (
-    <Container content={
-        <PlaceholderSegment iconName="hourglass half" message="Please wait." extraContent="We're fetching your assessment details." />
-      }
-    />
-  )
+  function renderContent() {
+    if (fetchOptions.fetching) return (
+      <PlaceholderSegment iconName="hourglass half" message="Please wait." extraContent="We're fetching your assessment details." />
+    )
 
-  if (fetchOptions.error !== "") return (
-    <Container content={
-        <PlaceholderSegment iconName="close" message="Error." extraContent={fetchOptions.error} />
-      }
-    />
-  )
+    if (fetchOptions.error !== "") return <PlaceholderSegment iconName="close" message="Error." extraContent={fetchOptions.error} />
 
-  if (peersToMark.length === 0) {
-    return (
-      <Container
-        content={
-          <PlaceholderSegment
-            iconName="checkmark"
-            message="Nobody to mark"
-            extraContent={
-              <>
-                <p>You have either completed marking or the student(s) you are marking have not completed the assessment.</p>
-                <Link href="/dashboard" >
-                  <Button content="Back to Dashboard" primary />
-                </Link>
-              </>
-            }
-          />
+    if (peersToMark.length === 0 && user.userType === "student") return (
+      <PlaceholderSegment
+        iconName="checkmark"
+        message="Nobody to mark"
+        extraContent={
+          <>
+            <p>You have either completed marking or the student(s) you are marking have not completed the assessment.</p>
+            <Link href="/dashboard" >
+              <Button content="Back to Dashboard" primary />
+            </Link>
+          </>
         }
       />
     )
-  }
 
-  if (completed) {
-    return (
-      <Container
-        content={
-          <PlaceholderSegment
-            iconName="checkmark"
-            message="Completed"
-            extraContent={
-              <>
-                <p>You have completed marking your peers.</p>
-                <Link href="/dashboard" >
-                  <Button content="Back to Dashboard" primary />
-                </Link>
-              </>
-            }
-          />
+    if (completed) return (
+      <PlaceholderSegment
+        iconName="checkmark"
+        message="Completed"
+        extraContent={
+          <>
+            <p>You have completed marking your peers.</p>
+            <Link href="/dashboard" >
+              <Button content="Back to Dashboard" primary />
+            </Link>
+          </>
         }
       />
+    )
+
+    return (
+      <>
+        <Metadata title={markingDetails.assessment.name} />
+        <Form onSubmit={handleSubmit} loading={submitting} >
+          <Segment.Group raised>
+            <Segment content={
+                <Header
+                  content={markingDetails.assessment.name}
+                  subheader={`Marking student ${peerIndex + 1} out of ${peersToMark.length}`}
+                  size="huge"
+                />
+              }
+            />
+            <Segment>
+              <Accordion
+                panels={[
+                  {
+                    key: "briefDescription",
+                    title: "Brief Description",
+                    content: markingDetails.assessment.briefDescription === "" ?
+                      "No brief description provided." :
+                      textToHTML(markingDetails.assessment.briefDescription),
+                  },
+                  {
+                    key: "description",
+                    title: "Assessment Description",
+                    content: markingDetails.assessment.description === "" ?
+                      "No assessment description provided." :
+                      textToHTML(markingDetails.assessment.description)
+                  },
+                  {
+                    key: "markingDescription",
+                    title: "Marking Description",
+                    content: markingDetails.assessment.markingDescription == "" ?
+                      "No marking description provided." :
+                      textToHTML(markingDetails.assessment.markingDescription),
+                  },
+                ]}
+                defaultActiveIndex={[2]}
+                exclusive={false}
+              />
+            </Segment>
+            <Segment>
+              <Header
+                content="Assessment Questions"
+                subheader="Marking criteria for specific assessment questions."
+              />
+              <MarkingQuestions
+                data={{
+                  questions: markingDetails.assessment.questions,
+                  markingCriteria: markingDetails.assessment.markingCriteria,
+                  answers: peersToMark[peerIndex].answers,
+                }}
+                onSubmit={handleSubmit}
+                onInput={handleMarkingQuestionsInput}
+                preview={user.userType === "teacher"}
+              />
+            </Segment>
+            <Segment content={`${getMarks()}/${getMarks(true)} marks allocated`}/>
+            <Segment>
+              <Header
+                content="General Marking Questions"
+                subheader="Marking criteria for the assessment as a whole."
+              />
+              <GeneralMarkingQuestions
+                questions={markingDetails.assessment.markingCriteria.general}
+                onInput={handleGeneralMarkingQuestionsInput}
+                preview={user.userType === "teacher"}
+              />
+            </Segment>
+            <Segment>
+              <Form.Group widths="equal" >
+                <input type="submit" style={{ display: "none" }} disabled/>
+                <Form.Button
+                  content="Exit"
+                  onClick={e => {
+                    e.preventDefault();
+                    if (confirm("Are you sure you want to exit? Un-submitted content will be discarded.")) window.location.href = "/dashboard";
+                  }}
+                  negative
+                  fluid
+                />
+                <Form.Button
+                  type="submit"
+                  content="Submit and Continue"
+                  primary
+                  fluid
+                />
+              </Form.Group>
+            </Segment>
+          </Segment.Group>
+        </Form>
+      </>
     )
   }
 
   return (
-    <Container>
-      <Metadata title={markingDetails.assessment.name} />
-      <Form onSubmit={handleSubmit} loading={submitting} >
-        <Segment.Group raised>
-          <Segment content={
-              <Header
-                content={markingDetails.assessment.name}
-                subheader={`Marking student ${peerIndex + 1} out of ${peersToMark.length}`}
-                size="huge"
-              />
-            }
-          />
-          <Segment>
-            <Accordion
-              panels={[
-                {
-                  key: "briefDescription",
-                  title: "Brief Description",
-                  content: markingDetails.assessment.briefDescription === "" ?
-                    "No brief description provided." :
-                    textToHTML(markingDetails.assessment.briefDescription),
-                },
-                {
-                  key: "description",
-                  title: "Assessment Description",
-                  content: markingDetails.assessment.description === "" ?
-                    "No assessment description provided." :
-                    textToHTML(markingDetails.assessment.description)
-                },
-                {
-                  key: "markingDescription",
-                  title: "Marking Description",
-                  content: markingDetails.assessment.markingDescription == "" ?
-                    "No marking description provided." :
-                    textToHTML(markingDetails.assessment.markingDescription),
-                },
-              ]}
-              defaultActiveIndex={[2]}
-              exclusive={false}
-            />
-          </Segment>
-          <Segment>
-            <Header
-              content="Assessment Questions"
-              subheader="Marking criteria for specific assessment questions."
-            />
-            <MarkingQuestions
-              data={{
-                questions: markingDetails.assessment.questions,
-                markingCriteria: markingDetails.assessment.markingCriteria,
-                answers: peersToMark[peerIndex].answers,
-              }}
-              onSubmit={handleSubmit}
-              onInput={handleMarkingQuestionsInput}
-              preview={false}
-            />
-          </Segment>
-          <Segment content={`${getMarks()}/${getMarks(true)} marks allocated`}/>
-          <Segment>
-            <Header
-              content="General Marking Questions"
-              subheader="Marking criteria for the assessment as a whole."
-            />
-            <GeneralMarkingQuestions
-              questions={markingDetails.assessment.markingCriteria.general}
-              onInput={handleGeneralMarkingQuestionsInput}
-              preview={false}
-            />
-          </Segment>
-          <Segment>
-            <Form.Group widths="equal" >
-              <input type="submit" style={{ display: "none" }} disabled/>
-              <Form.Button
-                content="Exit"
-                onClick={e => {
-                  e.preventDefault();
-                  if (confirm("Are you sure you want to exit? Un-submitted content will be discarded.")) window.location.href = "/dashboard";
-                }}
-                negative
-                fluid
-              />
-              <Form.Button
-                type="submit"
-                content="Submit and Continue"
-                primary
-                fluid
-              />
-            </Form.Group>
-          </Segment>
-        </Segment.Group>
-      </Form>
+    <Container style={{ padding: "2em 0" }}>
+      {renderContent()}
     </Container>
   )
 }
