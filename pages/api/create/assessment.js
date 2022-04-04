6291687c57d1ece5718c905de82d0aceba492077
FreetimeFM <awesomea380@tutanota.com>
2022-04-04 14:40:53 +0100

    Improved uploading peer assignments

--- pages/api/create/assessment.js
+++ pages/api/create/assessment.js
@@ -58,12 +58,18 @@ function assignPeerMarking(studentRefIDs, peerMarkingQuantity) {
     });
   }
 
-  return assignments.map(assignment => {
-    return {
-      userRefID: assignment[0],
-      peers: assignment.slice(1)
-    }
+  let peerList = [];
+
+  assignments.forEach(assignment => {
+    assignment.forEach((refID, index) => {
+      if (index !== 0) peerList.push({
+        marker: assignment[0],
+        peer: refID,
+      })
+    });
   });
+
+  return peerList;
 }
 
 // Adapted from https://stackoverflow.com/a/2450976
